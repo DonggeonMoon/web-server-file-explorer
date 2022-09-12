41d0a5cc0379cede1d7a46faec6d760f2c82439e
DonggeonMoon <nullsafer@naver.com>
2022-09-12 20:20:07 +0900

    fix: 파일 배포를 위한 수정

--- src/main/java/com/mdg/webserverfileexplorer/FileExplorer.java
+++ src/main/java/com/mdg/webserverfileexplorer/FileExplorer.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +16,12 @@ import java.io.IOException;
 
 @Controller
 public class FileExplorer {
+    @GetMapping("/")
+    @ResponseBody
+    public String test() {
+        return "테스트";
+    }
+
     @GetMapping("/fileExplorer")
     public String fileExplorerGet() {
         return "frame";
